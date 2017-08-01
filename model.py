a317879f20270c5b6f1749455c42170a1dd5d209
Jesus-2nta-2lahi <amiiir14.2@gmail.com>
2017-08-01 10:24:16 +0300

    init

--- model.py
+++ model.py
@@ -3,10 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class YourModel(Base):
-    __tablename__  = 'yourmodel'
+class Storis(Base):
+    __tablename__  = 'storis'
     id             = Column(Integer, primary_key=True)
-    # ADD YOUR FIELD BELOW ID
+    author         = Column(String)
+    rating         = Column(Integer)
+    description    = Column(String)
+    pic_url        = Column(String)
 
 # IF YOU NEED TO CREATE OTHER TABLE 
 # FOLLOW THE SAME STRUCTURE AS YourModel
